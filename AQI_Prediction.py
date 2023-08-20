6b2ffba7361cc37753bedb856235e34dd6b8fb47
SG-73 <107569908+SG-73@users.noreply.github.com>
2023-08-20 07:20:27 +0530

    Add files via upload

--- AQI_Prediction.py
+++ AQI_Prediction.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import joblib
+import pickle
 
-xgb = joblib.load(open('C:/Users/HP/Documents/Major Project/aqi.pkl', 'rb'))
+xgb = pickle.load(open('aqi.pkl', 'rb'))
 
 def predict_AQI(data):
     col = ['PM2_5', 'PM10', 'NO', 'NO2', 'NOx', 'NH3', 'CO', 'SO2', 'O3', 'Benzene', 'Toluene', 'Xylene',
